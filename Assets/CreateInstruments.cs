4a367c10adc8b6fa1b33e1d9f16f7dc8dc4520c9
drb8w <drb8w@directbox.com>
2014-12-03 19:59:30 +0100

    bla

---
+++ Assets/CreateInstruments.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateInstruments : MonoBehaviour {
+
+	public Transform GuitarPrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		// TODO: find prefab
+	
+	}
+
+	/// <summary>
+	/// Unity Callback
+	/// OnGUI is called for rendering and handling GUI events.
+	/// </summary>
+	void OnGUI () 
+	{
+	
+		if (Input.GetButton ("G"))  // TODO: register button G
+		{
+			// create Guitar-Instance out of Guitar-Prefab
+			// Instantiate(
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
