6bc2cc6832667dffd0f4229647310be716c21878
oggs91 <dschoerk@gmx.at>
2014-12-03 19:27:10 +0100

    init

---
+++ Assets/InstrumentScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstrumentScript : MonoBehaviour {
+
+	public bool active = true;
+	public AudioSource source;
+	public float volume = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (source == null)
+			return;
+
+		if(volume > 0)
+			volume -= Time.deltaTime*0.05f;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			volume = 1;
+
+		source.volume = volume;
+
+		if (active && !source.isPlaying) 
+			source.Play();
+		else if(!active && source.isPlaying)
+			source.Pause();
+	}
+}
